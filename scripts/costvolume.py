e4f04c8b427471278f5b0dbbb64d0f54aaa2df8a
Bradley Collicott <bcollico@stanford.edu>
2022-03-11 03:26:28 -0800

    initial cost volume

--- scripts/costvolume.py
+++ scripts/costvolume.py
@@ -1,3 +1,31 @@
+import torch
 
-def assemble_cost_volume(warped_feature_maps):
-    raise NotImplementedError
+def assemble_cost_volume(warped_feature_maps, n_views):
+    """Assemble the cost volume using a variance-based cost metric. There is
+    ambiguity in the MVS paper whether the mean is obtained over all 32 
+    feature volume channels or if it is per channel. The implementation here is 
+    per channel."""
+    b, c, w, h = warped_feature_maps.size()
+
+    N = w * h * b
+
+    print(N)
+
+    mean = torch.sum(warped_feature_maps, (0, 2, 3)) / (N)
+
+    print(torch.reshape(mean, (1, c, 1, 1)).size())
+    print(warped_feature_maps.size())
+
+    diff = warped_feature_maps - torch.reshape(mean, (1, c, 1, 1))
+
+    var = torch.sum(diff**2, [2,3]) / N
+
+    return var
+
+if __name__ == "__main__":
+
+    input = torch.randn(9,32,1000,1000)
+
+    var = assemble_cost_volume(input, n_views)
+
+    print(var)
